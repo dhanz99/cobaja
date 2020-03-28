50be7271d291697b580c070f55ea99c8cc2c725b
hirdhan <mhirdhanaghnia@gmail.com>
2020-03-28 11:20:59 +0700

    Add MainActicity.Java

--- app/src/main/java/com/example/cobaja/MainActivity.java
+++ app/src/main/java/com/example/cobaja/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.cobaja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    DatePickerDialog pickerDialog;
+    EditText editText;
+    Button btn;
+    TextView tvw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvw=(TextView)findViewById(R.id.editText2);
+        editText=(EditText) findViewById(R.id.editText2);
+        editText.setInputType(InputType.TYPE_NULL);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                pickerDialog = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                editText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                pickerDialog.show();
+            }
+        });
+        btn=(Button)findViewById(R.id.btn2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvw.setText("Selected Date: "+ editText.getText());
+            }
+        });
+
+        Button One = (Button) findViewById(R.id.btn2);
+        One.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),editText.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
